0a1c22c4b621bbaa578a2445b90e1fe993136a26
albertoferri <falberto99@gmail.com>
2024-02-02 15:23:15 +0100

    snack 3    inserisco prompt nel ciclo for

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -3,3 +3,14 @@
 // Chiedi per 6 volte all’utente di inserire un numero,
 // Solo se è dispari inseriscilo nell’array.
 // Stampa a schermo il contenuto dell'array
+
+// Crea un array vuoto.
+const userArray = [];
+
+
+for (let i = 1; i <= 6; i++){
+    // inserisco un prompt da ripetere 
+    userArray.push(prompt("Inserisci un numero"));
+
+}
+console.log(userArray);
