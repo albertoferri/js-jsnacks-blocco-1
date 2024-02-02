31792a0878e485df2b0f885a2c5e512155aa0e02
albertoferri <falberto99@gmail.com>
2024-02-02 17:02:32 +0100

    snack 5         creata variabile tramitre prompt e resa subito un numero.
    impostato un ciclo for in grado di stampare su console il cubo di n numeri

--- snack5/js/script.js
+++ snack5/js/script.js
@@ -2,3 +2,14 @@
 // Stampa il cubo dei primi N numeri,
 // (partendo da 1, il numero N indicato dall'utente è compreso)
 // dove N è un numero indicato dall’utente.
+
+// Chiedi all'utente di inserire un numero
+const numberChoice = Number(prompt("Inserisci un numero:"));
+
+// Calcola e stampa il cubo dei primi N numeri attribuiendo al valore la variabile i che andra ad aggiurnarsi per il numero di volte
+for (let i = 1; i <= numberChoice; i++) {
+    // Stampa il cubo dei primi N numeri
+    let cubo = i ** 3;
+    console.log(`Il cubo di ${i} è: ${cubo}`);
+}
+
