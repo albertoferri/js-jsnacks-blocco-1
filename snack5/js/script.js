c43c5080c33db13998239a0776ae275f23678f11
albertoferri <falberto99@gmail.com>
2024-02-02 17:05:01 +0100

    snack 5     creata validazione input (se è un numero e se è maggiore di 0)

--- snack5/js/script.js
+++ snack5/js/script.js
@@ -6,10 +6,17 @@
 // Chiedi all'utente di inserire un numero
 const numberChoice = Number(prompt("Inserisci un numero:"));
 
-// Calcola e stampa il cubo dei primi N numeri attribuiendo al valore la variabile i che andra ad aggiurnarsi per il numero di volte
-for (let i = 1; i <= numberChoice; i++) {
-    // Stampa il cubo dei primi N numeri
-    let cubo = i ** 3;
-    console.log(`Il cubo di ${i} è: ${cubo}`);
-}
 
+// Verifico che il valore inserito sia un numero e che sia maggiore di zero
+if (!isNaN(numberChoice) && numberChoice > 0) {
+    // Calcola e stampa il cubo dei primi N numeri attribuiendo al valore la variabile i che andra ad aggiurnarsi per il numero di volte
+    for (let i = 1; i <= numberChoice; i++) {
+
+        // Stampa il cubo dei primi N numeri,
+        let cubo = i ** 3;
+        console.log(`Il cubo di ${i} è: ${cubo}`);
+    }
+} else {
+    // messaggio di errore
+    alert("Inserisci un numero valido e maggiore di zero.");
+}
