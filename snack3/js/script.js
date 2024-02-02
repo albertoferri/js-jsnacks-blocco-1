eb4e004c6fd8ef8fa3a0d744bdff36855d342470
albertoferri <falberto99@gmail.com>
2024-02-02 15:33:57 +0100

    snack 3            commit con errori

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -7,10 +7,23 @@
 // Crea un array vuoto.
 const userArray = [];
 
+// Chiedi per 6 volte all’utente di inserire un numero,
+
 
 for (let i = 1; i <= 6; i++){
     // inserisco un prompt da ripetere 
-    userArray.push(prompt("Inserisci un numero"));
+    let userChoice = prompt("Inserisci un numero");
+
+    // Solo se è dispari inseriscilo nell’array.
+    if (userChoice % 2 !== 0){
+
+        userArray.push(userChoice)
+
+        console.log(userChoice)
+    }
+
+     
+    // userArray.push( Number(prompt("Inserisci un numero")) );
 
 }
 console.log(userArray);
