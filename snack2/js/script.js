5c4b0fcdf81af16afd47282891e52b9c34292954
albertoferri <falberto99@gmail.com>
2024-02-02 15:08:37 +0100

    snack2    stampo il risultato in pagina

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -3,12 +3,17 @@
 // Il programma stampa la somma di tutti i numeri inseriti.
 
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// imposto variabile somma a 0
 let somma = 0;
 
+// utilizzo un ciclo for per chiedere 10 volte all'utente di mettere un numero
 for (let i = 1; i <= 10; i++){
-    let userInput = Number(prompt("inserisci un numero."));
-
-    somma += parseFloat(userInput);
+    // prompt utente
+    let userInput = Number(prompt("Inserisci un numero"));
     
+    // Converto l'input dell'utente in un numero e lo aggiunge alla somma
+    somma += parseFloat(userInput);
 }
-console.log("La somma dei dieci numeri inseriti è: " + somma);
+
+// stampo in pagina il risultato
+document.getElementById("somma").innerHTML = "La somma dei dieci numeri inseriti è: " + somma;
