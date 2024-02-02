d49e3debad8aa16ac620a6f60e11343f9573cabd
albertoferri <falberto99@gmail.com>
2024-02-02 13:24:59 +0100

    aggiunto controllo della lunghezza

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -4,16 +4,17 @@
 
 // L’utente inserisce due parole in successione, con due prompt.
 const firstWord = prompt("Inserisci la prima parola");
-console.log(firstWord.length)
-
 const secondWord = prompt("Inserisci la seconda parola");
-console.log(secondWord.length)
 
-// stampo in pagina le due parole inserite
-document.getElementById("1st-span").innerHTML = firstWord;
-document.getElementById("2nd-span").innerHTML = secondWord;
-
-
-
-
-console.log(firstWord, secondWord);
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+// aggiungo controllo della lugnghezza dei caratteri
+if (firstWord.length < secondWord.length){
+    document.getElementById("1st-span").innerHTML = firstWord;
+} else {
+    document.getElementById("1st-span").innerHTML = secondWord;
+}
+if (firstWord.length > secondWord.length){
+    document.getElementById("2nd-span").innerHTML = firstWord;
+} else {
+    document.getElementById("2nd-span").innerHTML = secondWord;
+}
