b5ea3bf4924b812f14f773575833c9d23ae3d86f
albertoferri <falberto99@gmail.com>
2024-02-02 13:23:23 +0100

    Snack 1: realizzazione dei due prompt e stampo in pagina degli elementi.

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -1,3 +1,19 @@
 // Snack 1:
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// L’utente inserisce due parole in successione, con due prompt.
+const firstWord = prompt("Inserisci la prima parola");
+console.log(firstWord.length)
+
+const secondWord = prompt("Inserisci la seconda parola");
+console.log(secondWord.length)
+
+// stampo in pagina le due parole inserite
+document.getElementById("1st-span").innerHTML = firstWord;
+document.getElementById("2nd-span").innerHTML = secondWord;
+
+
+
+
+console.log(firstWord, secondWord);
