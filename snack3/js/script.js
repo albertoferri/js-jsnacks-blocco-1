44729fbb2985a7882c49358fa0cba18ea3abdc54
albertoferri <falberto99@gmail.com>
2024-02-02 15:12:36 +0100

    INIZIO SNACK 3

---
+++ snack3/js/script.js
@@ -0,0 +1,5 @@
+// Snack 3:
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// Solo se è dispari inseriscilo nell’array.
+// Stampa a schermo il contenuto dell'array
