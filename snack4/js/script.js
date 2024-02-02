f99f5f0d57eb007f40ca653f36e0ce909a9309ef
albertoferri <falberto99@gmail.com>
2024-02-02 16:05:07 +0100

    snack 4       imposto variabile con prompt

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -1,3 +1,10 @@
 // Snack 4:
 // Chiedi un numero di 4 cifre all’utente
 // e calcola la somma di tutte le cifre che compongono il numero.
+
+
+// Chiedi un numero di 4 cifre all’utente
+let userNumber = Number( prompt("Inserisci un numero:"));
+
+// Verifico che il numero sia valido
+// e calcola la somma di tutte le cifre che compongono il numero.
