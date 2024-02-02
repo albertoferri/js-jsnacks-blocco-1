bd0b1dffa41eba14d2c75963983c5caa64b5d193
albertoferri <falberto99@gmail.com>
2024-02-02 16:14:07 +0100

    snack 4       verifico se il valore è corretto e sommo le singole cifre del numero

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -8,3 +8,20 @@ let userNumber = Number( prompt("Inserisci un numero:"));
 
 // Verifico che il numero sia valido
 // e calcola la somma di tutte le cifre che compongono il numero.
+if (!isNaN(userNumber)) {
+    // Creo variabile settata a 0
+    let sommaInt = 0;
+
+    // si usa mettere il ";" quando non c'è bisogno di inizializzare il ciclo for 
+    // dato che in questo caso avevamo inserito la variabile sopra (e quinddi fuori)
+    for (; userNumber !== 0; userNumber = Math.floor(userNumber / 10)) {
+        // aggiungo l'ultima cifra dell'userNumber alla variabile sommaInt
+        sommaInt += userNumber % 10;
+    }
+
+    // stampo in pagina il risultato
+    document.getElementById("numbers").innerHTML = "La somma delle cifre è: " + sommaInt;
+} else {
+    // messaggio di errore
+    alert("Inserisci un numero valido.");
+}
